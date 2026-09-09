b697e98da8cbe14b36996918e02e123249370ef9
Caique Marcelino Souza <caiquemarcelinosouza@gmail.com>
2026-09-09 00:10:28 -0300

    Remove trash file

--- trash.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "vendor/autoload.php";
-$SSLWS_PROXY_KEY = "cGEldU8vN0g1VWAvODZvRmsrfnxjcz5PWjknY0k5Ljg6XVgpeDA9Iyx1dCxsbFU0TDI";
-$SSLWS_BASE_URL = "https://api.prxsrv2.gbix.com.br/";
-$proxy = new \SslwsProxy\Client\ProxyClient($SSLWS_BASE_URL, $SSLWS_PROXY_KEY);
-var_dump($proxy->checkSsl("https://google.com")->getBody()->getContents());
